8ef56d5a8a1f3b15ffbaf5c8674cfad09463edac
Akshay N <akshaynigam405@gmail.com>
2026-07-15 11:22:29 +0530

    removed Createresume

--- Createresume/route/route.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { createresume } from "../controller/usercontroller.js";
-
-//this file contains the create-resume api 
-const router=express.Router()
-router.post("/createresume", createresume);
-
-export default router
