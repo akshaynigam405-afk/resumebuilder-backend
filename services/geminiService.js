c4537574976840cbd905daf15eed149ca2fe7b6d
Akshay N <akshaynigam405@gmail.com>
2026-07-16 11:17:29 +0530

    changed the gemini API from 2.5 to 3.5

--- services/geminiService.js
+++ services/geminiService.js
@@ -17,7 +17,7 @@ export const generateResumeSummary = async(resumeData) => {
         const prompt = resumeSummaryPrompt(resumeData);
 
         const response = await ai.models.generateContent({
-            model: "gemini-2.5-flash",
+            model: "gemini-3.5-flash",
             contents: prompt,
         });
 
